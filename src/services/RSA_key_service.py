bb1d7c77d1dbef0567d5cede66ccef4c3a6672be
jhakkari <juuso.hakkari@helsinki.fi>
2023-11-11 20:54:15 +0200

    Add pylint

--- src/services/RSA_key_service.py
+++ src/services/RSA_key_service.py
@@ -2,7 +2,7 @@ import random
 
 
 class KeyService:
-    
+
     def miller_rabin_primality_test(self, n, k=40):
         """Determines whether a given integer is likely to be a prime number
 
@@ -15,9 +15,14 @@ class KeyService:
             False (bool): If n is composite
         """
 
-        if n < 2: return False
-        if n < 4: return True
-        if n % 2 == 0: return False
+        if n < 2:
+            return False
+
+        if n < 4:
+            return True
+
+        if n % 2 == 0:
+            return False
 
         s = 0
         d = n-1
@@ -28,9 +33,13 @@ class KeyService:
         for i in range(k):
             a = random.randrange(2, n-1)
             x = (a**d) % n
-            if x == 1: continue
+            if x == 1:
+                continue
+
             for j in range(s):
-                if x == n-1: break
+                if x == n-1:
+                    break
+
                 x = (x * x) % n
             else:
                 return False
