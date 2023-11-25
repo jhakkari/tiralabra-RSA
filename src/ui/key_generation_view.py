54267e1f456ee3dc8f1b06b91f69a3d9c14c52c1
jhakkari <juuso.hakkari@helsinki.fi>
2023-11-25 22:53:45 +0200

    Add GUI

--- src/ui/key_generation_view.py
+++ src/ui/key_generation_view.py
@@ -1,9 +1,13 @@
-from tkinter import ttk, constants
+from tkinter import ttk, constants, Text
+from services.rsa_key_service import KeyService
 
 class KeyGenerationView:
     def __init__(self, root):
         self._root = root
         self._frame = None
+        self._public_key_textbox = None
+        self._private_key_textbox = None
+        self._key_service = KeyService()
 
         self._initialize()
 
@@ -12,11 +16,26 @@ class KeyGenerationView:
 
     def destroy(self):
         self._frame.destroy()
+    
+    def _handle_generate_keys(self):
+        keys = self._key_service.generate()
+        self._public_key_textbox.insert("end-1c", f"{keys[0]}")
+        self._private_key_textbox.insert("end-1c", f"{keys[1]}")
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        label = ttk.Label(master=self._frame, text="Nothing in here, yet!")
+        public_key_label = ttk.Label(master=self._frame, text="Public key:")
+        private_key_label = ttk.Label(master=self._frame, text="Private key:")
 
-        label.grid(row=0, column=0, columnspan=2)
-        self._frame.rowconfigure(0, minsize=200, weight=1)
-        self._frame.columnconfigure(0, minsize=200, weight=1)
+        self._public_key_textbox = Text(master=self._frame)
+        self._private_key_textbox = Text(master=self._frame)
+
+        generate_keys_button = ttk.Button(self._frame, text="Generate", command=self._handle_generate_keys)
+
+        public_key_label.grid(columnspan=2, sticky="ew", padx=5, pady=5)
+        self._public_key_textbox.grid(columnspan=2, sticky="ew", padx=5, pady=5)
+        private_key_label.grid(columnspan=2, sticky="ewns", padx=5, pady=5)
+        self._private_key_textbox.grid(columnspan=2, sticky="ew", padx=5, pady=5)
+        generate_keys_button.grid(columnspan=2, sticky="ewns", padx=5, pady=5)
+
+        self._frame.grid_columnconfigure(1, weight=1)
