4457570290954b20c6836792d0c9e94ff91ea384
jhakkari <juuso.hakkari@helsinki.fi>
2023-11-25 22:53:16 +0200

    Add key creation

--- src/services/rsa_key_service.py
+++ src/services/rsa_key_service.py
@@ -45,7 +45,7 @@ class KeyService:
                 return False
         return True
 
-    def extended_gcd(self, a, b):
+    def extended_ecd(self, a, b):
         """Calculates the greatest common divisor for given integers and coefficients x,y such that ax + by = gcd(a,b)
 
         Args:
@@ -81,3 +81,74 @@ class KeyService:
 
         gcd = old_r
         return (gcd, old_s, old_t, t, s)
+
+    def find_prime(self, b=17):
+        """Generates and tests large odd numbers until prime is found
+
+        Args:
+            b (int): The number of bits of the wanted result
+
+        Returns:
+            random_int (int): A strong propable prime number
+        """
+
+        while True:
+            random_int = random.randint(2**(b-1), 2**b-1)
+            if random_int % 2 == 0:
+                continue
+
+            if self.miller_rabin_primality_test(random_int):
+                return random_int
+
+    def lcm(self, a, b):
+        """Calculates the least common multiple of two integers
+
+        Args:
+            a (int): First integer
+            b (int): Second integer
+
+        Returns:
+            lcm (int): Least common multiple of a and b
+        """
+
+        gcd = self.extended_ecd(a, b)[0]
+        lcm = int(abs(a) * (abs(b) / gcd))
+        return lcm
+
+    def choose_e(self, an):
+        """Chooses an integer e such that 2 < e < an, e and an are coprime
+
+        Args:
+            an (int): λ(n), where λ is Carmichaels totient function
+
+        Returns:
+            e (int): Chosen value for e
+        """
+        # 2 < e < a(n) and gcd(e, a(n)) = 1
+        # Most commonly used: return 2**16+1
+        e = random.randint(3, an - 1)
+        while self.extended_ecd(e, an)[0] != 1:
+            e = random.randint(3, an - 1)
+
+        return e
+
+    def find_d(self, e, an):
+        return self.extended_ecd(e, an)[1] + an
+
+    def generate(self):
+        """Generates public and private keys
+
+        Returns:
+            (n, e) (int): public key
+            (n, d) (int): private key
+        """
+
+        p = self.find_prime()
+        q = self.find_prime()
+        n = p * q
+
+        an = self.lcm(p - 1, q - 1)
+        e = self.choose_e(an)
+        d = self.find_d(e, an)
+
+        return ((n, e), (n, d))
