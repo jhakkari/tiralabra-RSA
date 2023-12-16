3065bfee74365fc580df70861e5bbf722fc2ce94
jhakkari <juuso.hakkari@helsinki.fi>
2023-12-16 22:52:47 +0200

    Add decryption

--- src/services/rsa_cipher_service.py
+++ src/services/rsa_cipher_service.py
@@ -5,8 +5,17 @@ class CipherService:
         encoded_message = self.encode(message)
         encrypted_message = [pow(c, public_exp, modulus) for c in encoded_message]
 
-        return ''.join(str(encrypted_message))
+        return encrypted_message
 
     def encode(self, message):
         return [ord(c) for c in message]
+
+    def decrypt(self, private_exp, modulus, ciphered_message):
+        message = [pow(c, private_exp, modulus) for c in ciphered_message]
+        decoded_message = self.decode(message)
+
+        return decoded_message
+
+    def decode(self, message):
+        return "".join(chr(c) for c in message)
 
